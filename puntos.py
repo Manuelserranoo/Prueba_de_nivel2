5602d7c0ffe79f0b8eba4a03b2122ba6c8599067
Manoloserranoo <manuel.serranovallejo@usp.ceu.es>
2023-03-23 08:52:54 +0100

    empezar

---
+++ puntos.py
@@ -0,0 +1,15 @@
+class Punto():
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+    def __str__(self):
+        return "({},{})".format(self.x,self.y)
+    def crear(self):
+        x = int(input("introduce la coordenada x: "))
+        ###
+    def sum(self):
+        return self.x + self.y
+    def coordenada(self):
+        
+    
+        
