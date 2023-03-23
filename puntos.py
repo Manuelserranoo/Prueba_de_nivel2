a2de77811db96af4fca3ccc2cade6d7a27bc4b1f
Manoloserranoo <manuel.serranovallejo@usp.ceu.es>
2023-03-23 09:11:11 +0100

    importar math

--- puntos.py
+++ puntos.py
@@ -1,15 +1,31 @@
+import math 
 class Punto():
     def __init__(self,x,y):
         self.x = x
         self.y = y
     def __str__(self):
-        return "({},{})".format(self.x,self.y)
-    def crear(self):
-        x = int(input("introduce la coordenada x: "))
-        ###
+        return f'({self.x}, {self.y})'
     def sum(self):
         return self.x + self.y
     def coordenada(self):
+        if self.x > 0 and self.y > 0:
+            return "Primer cuadrante"
+        elif self.x < 0 and self.y > 0:
+            return "Segundo cuadrante"
+        elif self.x < 0 and self.y < 0:
+            return "Tercer cuadrante"
+        elif self.x > 0 and self.y < 0:
+            return "Cuarto cuadrante" 
+        elif self.x == 0 and self.y != 0:
+            return "El punto esta sobre el eje de las X"
+        elif self.x != 0 and self.y == 0:
+            return "El punto esta sobre el eje de las Y"
+        else:
+            return "Punto de origen"
+    def vector(self,p2):
+        return "El vector es:" f'({self.x - p2.x},{self.y - p2.y})'
+    def distancia(self,p2):
+        return "La dist es:" f'({sqrt()})'
         
     
         
