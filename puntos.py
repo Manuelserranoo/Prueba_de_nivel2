fb3787841f3e7433747ee6d5d57e6cb9e7232a20
Manoloserranoo <manuel.serranovallejo@usp.ceu.es>
2023-03-23 09:40:24 +0100

    crear clase rectangulo y metodos

--- puntos.py
+++ puntos.py
@@ -25,8 +25,13 @@ class Punto():
     def vector(self,p2):
         return "El vector es: " f'({abs(self.x - p2.x)},{abs(self.y - p2.y)})'
     def distancia(self,p2):
-        return "La dist es: " f'({math.sqrt((abs(self.x - p2.x)**2) + abs(self.y - p2.y)**2)})'
-    
+        return "La dist es: " f'{math.sqrt((abs(self.x - p2.x)**2) + abs(self.y - p2.y)**2)}'
+class Rectangulo(Punto):
+    def __init__(self,p1,p2):
+        self.p1 = p1
+        self.p2 = p2
+    def base(self):
+        return "La base es igual a: " f'({self.p1.x - self.p2.x})' 
 def main():
     A = Punto(2,3)
     B = Punto(5,5)
@@ -41,6 +46,14 @@ def main():
     print(B.vector(A))
     print(A.distancia(B))
     print(B.distancia(A))
+    if A.distancia(D) > B.distancia(D) and A.distancia(D) > C.distancia(D):
+        return ("A es el más cercano a D")
+    if B.distancia(D) > A.distancia(D) and B.distancia(D) > C.distancia(D):
+        print("B es el más cercano a D")
+    else:
+        return ("C es el más cercano a D")
+    rectangulo = Rectangulo(B,C)
+    print(rectangulo.base())
 if __name__ == "__main__":
     main()
         
